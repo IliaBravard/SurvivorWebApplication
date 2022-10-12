f8193286a5a8f1f42d2409aed0f96ba26a80295c
IliaBravard <igbravard@dmacc.edu>
2022-10-11 23:22:13 -0500

    index.html Page Created
    I added an "index.html" page with a simple form where the user can add a new season record to the "seasons" table.

---
+++ src/controller/AddSeasonServlet.java
@@ -0,0 +1,62 @@
+package controller; // The package where this servlet is located at
+
+// Including the needed imports
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Seasons;
+
+/**
+ * Servlet implementation class AddSeasonServlet. This servlet class adds a
+ * season record to the "seasons" table in the database.
+ */
+@WebServlet("/addSeasonServlet")
+public class AddSeasonServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * This is the default, no argument constructor.
+	 * 
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AddSeasonServlet() {
+	}
+
+	/**
+	 * This method adds a new season record to the "seasons" table.
+	 * @param request - the HTTP request
+	 * @param response - the HTTP response
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		// Getting the input from the user
+		String seasonStringNum = request.getParameter("number");
+		String seasonName = request.getParameter("name");
+		String seasonWinner = request.getParameter("winner");
+		String dateString = request.getParameter("date");
+		
+		// Parsing some of it to be acceptable for the constructor
+		int seasonNum = Integer.parseInt(seasonStringNum);
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("mm/dd/yyyy");
+		LocalDate firstAired = LocalDate.parse(dateString, format);
+		
+		// Actually adding the new season
+		Seasons toAdd = new Seasons(seasonNum, seasonName, seasonWinner, firstAired);
+		SeasonsDAO sh = new SeasonsDAO();
+		sh.insertSeason(toAdd);
+		
+		// Forwarding the request to the appropriate page
+		getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+	}
+
+}
