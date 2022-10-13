ac5e90a46c8cd78c929cfe4624a1746c7c0405ee
IliaBravard <igbravard@dmacc.edu>
2022-10-12 22:07:08 -0500

    Created a Local Date Attribute Converter
    Created a LocalDateAttributeConverter for the "seasons" table (the date when it first aired) so that the local database instance does not treat it as a "BLOB" object.

---
+++ src/controller/LocalDateAttributeConverter.java
@@ -0,0 +1,43 @@
+package controller; // The package where this converter is located at
+
+// Including the needed imports
+import java.sql.Date;
+import java.time.LocalDate;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/**
+ * This class converts the LocalDate field of the 'Seasons' model class to an
+ * understandable date data type for SQL. Otherwise, it will be represented as a
+ * BLOB object in the actual database. This class also implements an interface
+ * to accomplish the given task.
+ */
+
+@Converter(autoApply = true)
+public class LocalDateAttributeConverter implements AttributeConverter<LocalDate, Date> {
+
+	/**
+	 * This method converts the value stored in the entity attribute into the data
+	 * representation to be stored in the database.
+	 * 
+	 * @param attribute - the data attribute
+	 * @return the data representation of the entity attribute, if any
+	 */
+	@Override
+	public Date convertToDatabaseColumn(LocalDate attribute) {
+		return (attribute == null ? null : Date.valueOf(attribute));
+	}
+
+	/**
+	 * This method converts the attribute to an understandable local data for the
+	 * database used.
+	 * 
+	 * @param dbData - the attribute data to be converted
+	 * @return the converted date
+	 */
+	@Override
+	public LocalDate convertToEntityAttribute(Date dbData) {
+		return (dbData == null ? null : dbData.toLocalDate());
+	}
+}
